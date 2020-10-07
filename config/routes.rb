73700f28bc2495b20e70a9f2e89423aaafd7421d
C0mbed <matt@farncombe.co>
2020-10-07 14:41:23 -0600

    add nested workout routes to user#resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create', as: 'sessions'
-  resources :users, only: [:create, :show, :new, :edit, :update]
+
+  resources :users, only: [:create, :show, :new, :edit, :update] do
+    resources :workouts, only: [:show. :index]
+  end
+
   resources :workouts
 end
