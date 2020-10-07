5bfc9eeca6a8c14d16a2f396f10a1cbd7fbae427
C0mbed <matt@farncombe.co>
2020-10-06 20:49:33 -0600

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { in: 8..20}
+
 end
