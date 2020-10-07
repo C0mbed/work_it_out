2137ee5a520ce88193a4744d64f5337551e2a1c7
C0mbed <matt@farncombe.co>
2020-10-07 05:48:20 -0600

    added workouts model and updated the associations on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
-  validates :password, length: { in: 8..20}
+  validates :password, length: { in: 8..20 }
+
+  has_many :workouts
 
 end
