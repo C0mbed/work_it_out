dad8f65b6de7f8a8415e2980cd870f55a3d141d8
C0mbed <matt@farncombe.co>
2020-10-07 08:55:56 -0600

    create workout migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_120537) do
+ActiveRecord::Schema.define(version: 2020_10_07_145431) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2020_10_06_120537) do
     t.string "email"
     t.string "password_digest"
     t.boolean "admin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "title"
+    t.string "difficulty"
+    t.integer "minutes"
+    t.string "type"
+    t.integer "rating"
+    t.integer "likes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
