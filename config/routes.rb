74149d16e2e0a227f70b3aec69ad95d5927548c5
C0mbed <matt@farncombe.co>
2020-10-06 20:06:27 -0600

    add login form to sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'signup'
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   post 'sessions', to: 'sessions#create', as: 'sessions'
