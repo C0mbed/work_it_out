7dcdabc084ee0973ce2edb1df48454f0ae0fab9f
C0mbed <matt@farncombe.co>
2020-10-19 19:47:43 -0600

    fix routing to exercise/repetition and workout_id

--- app/controllers/repetitions_controller.rb
+++ app/controllers/repetitions_controller.rb
@@ -1,8 +1,8 @@
 class RepetitionsController < ApplicationController
     def new
         @repetition = Repetition.new
-        @repetition.workout_id = params[:workout_id]
-        @repetition.exercise_id = params[:exercise_id]
+        @workout = Workout.find_by(id: params[:workout_id])
+        @exercise = Exercise.find_by(id: params[:exercise_id])
     end
 
     def index
@@ -10,14 +10,15 @@ class RepetitionsController < ApplicationController
 
     def create
         @repetition = Repetition.find_by(workout_id: repetitions_params[:workout_id])
+        byebug
         if !@repetitions.nil?
             redirect_to edit_workout_exercise_repetition_path
         else
           @repetition = Repetition.create(repetitions_params)
           if @repetition.save
-            @repetition = Repetition.find_by(workout_id: repetitions_params[:workout_id])
+            @repetition = Repetition.last
     
-            redirect_to workout_path(@repetition)
+            redirect_to workout_path(@repetition.workout_id)
           else
             redirect_to workouts_path
           end
