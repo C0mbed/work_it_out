86e683ba5f3e704fb7c31ec1804b68d547ba6783
C0mbed <matt@farncombe.co>
2020-10-21 05:35:40 -0600

    add validations to repetitions for presence and numericality only:integer

--- app/models/repetition.rb
+++ app/models/repetition.rb
@@ -1,5 +1,9 @@
 class Repetition < ActiveRecord::Base
   belongs_to :workout
   belongs_to :exercise
+  validates :reps, presence: true
+  validates :sets, presence: true
+  validates :reps, numericality: { only_integer: true }
+  validates :sets, numericality: { only_integer: true }
 
 end
